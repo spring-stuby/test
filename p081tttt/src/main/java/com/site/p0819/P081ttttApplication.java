5695e4e1ce267ba6e2970677f3d3c3bc5217eecd
한국직업전문학교 <@DESKTOP-QMK5PJF>
2021-08-25 09:42:01 +0900

    zz

---
+++ p081tttt/src/main/java/com/site/p0819/P081ttttApplication.java
@@ -0,0 +1,13 @@
+package com.site.p0819;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P081ttttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P081ttttApplication.class, args);
+	}
+
+}
---
+++ p081tttt/src/test/java/com/site/p0819/P081ttttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.site.p0819;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P081ttttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
